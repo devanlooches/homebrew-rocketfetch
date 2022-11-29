3104e5a028902617b9031cb982a1c7afa94b47b6
Devan Looches <devan.looches@gmail.com>
2022-11-28 16:34:58 -0800

    chore: Update sha256

--- Formula/rocketfetch.rb
+++ Formula/rocketfetch.rb
@@ -2,7 +2,7 @@ class Rocketfetch < Formula
   desc "command line system information tool written with multithreading in Rust for performance with toml file configuration"
   homepage "https://github.com/devanlooches/rocketfetch/tree/main"
   url "https://github.com/devanlooches/rocketfetch/releases/download/v0.7.2/rocketfetch-x86_64-apple-darwin.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "1bab5a6775ed26ddb385c1f4daa9bfe5cdc95b0eedb9258ab337fe35755718c0"
   version "0.7.2"
 
   def install
