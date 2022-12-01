948500b4de1320851f086487afa5ff3452494326
Devan Looches <devan.looches@gmail.com>
2022-11-30 20:51:58 -0800

    fix: Fix download link

--- Formula/rocketfetch.rb
+++ Formula/rocketfetch.rb
@@ -1,7 +1,7 @@
 class Rocketfetch < Formula
   desc "command line system information tool written with multithreading in Rust for performance with toml file configuration"
   homepage "https://github.com/devanlooches/rocketfetch/tree/main"
-  url "https://github.com/devanlooches/rocketfetch/releases/download/v0.7.2/rocketfetch-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/devanlooches/rocketfetch/releases/download/v0.7.2/rocketfetch-x86_64-apple-darwin-v0.7.2.tar.gz"
   sha256 "1bab5a6775ed26ddb385c1f4daa9bfe5cdc95b0eedb9258ab337fe35755718c0"
   version "0.7.2"
 
