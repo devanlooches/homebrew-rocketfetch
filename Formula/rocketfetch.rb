5969a49127e4794e953ca62c41ee2535ee8e7f41
runner <runner@Mac-1693100753395.local>
2023-08-27 01:49:01 +0000

    rocketfetch 0.7.4

--- Formula/rocketfetch.rb
+++ Formula/rocketfetch.rb
@@ -1,9 +1,9 @@
 class Rocketfetch < Formula
   desc "command line system information tool written with multithreading in Rust for performance with toml file configuration"
   homepage "https://github.com/devanlooches/rocketfetch/tree/main"
-  url "https://github.com/devanlooches/rocketfetch/releases/download/v0.7.3/rocketfetch-x86_64-apple-darwin-v0.7.3.tar.gz"
-  sha256 "bb6311837442e1f3fa6a893cbd5e97a1d1836da11338ca1df7f65f076add8a8a"
-  version "0.7.3"
+  url "https://github.com/devanlooches/rocketfetch/releases/download/v0.7.4/rocketfetch-x86_64-apple-darwin-v0.7.4.tar.gz"
+  sha256 "70f8e320d9a4c26804486908859e4beebf1559ee67615042ddeec106789e1257"
+  version "0.7.4"
 
   def install
     bin.install "rocketfetch"
